ca931b85140595e80b2c6edce27968818fc7d676
Morgan Schweers <cyberfox@gmail.com>
2008-12-28 04:13:12 -0800

    Add simple authentication, mostly as a place to hang an API key and lighthouse subdomain from.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
   helper :all # include all helpers, all the time
+  before_filter :get_token
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -11,5 +13,13 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+  private
+  def get_token
+    if logged_in?
+      Lighthouse.account = current_user.subdomain
+      Lighthouse.token = @token = current_user.api_key
+    end
+  end
 end
