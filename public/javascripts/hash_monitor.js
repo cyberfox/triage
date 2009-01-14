8146ffdffff0f767dcd33b27c5babc13b5d9af06
Morgan Schweers <cyberfox@gmail.com>
2009-01-14 13:53:11 -0800

    If there isn't an onHashChange method on a given page, don't waste time monitoring for hash changes that will never be reported.

--- public/javascripts/hash_monitor.js
+++ public/javascripts/hash_monitor.js
@@ -1,12 +1,16 @@
 var curHash='';
 
-setInterval("checkHash()", 1000);
+var hashCheckInterval = setInterval("checkHash()", 1000);
 
 function checkHash() {
   if(document.location.hash != curHash) {
     curHash = document.location.hash;
     if(window.onHashChange) {
       onHashChange(curHash.substr(1));
+    } else {
+      // If there isn't a onHashChange on the page, don't waste time
+      // looking for hash changes.
+      hashCheckInterval.cancelInterval();
     }
   }
 }
