72596422e4b337feef1e5b1e3eb1593f8cddde52
Morgan Schweers <cyberfox@gmail.com>
2009-01-28 00:22:48 -0800

    Make the UI do the right thing for create/edit forms, and not preserve info between the two.  The back end isn't listening correctly for the form post yet, though.

--- public/javascripts/triage.js
+++ public/javascripts/triage.js
@@ -32,13 +32,51 @@ function nuke(elem) {
   $(elem).remove();
 }
 
-function editBucketForm(tag, desc, milestone_id, state, boilerplate) {
+function showBucketForm() {
+  $$('.cancel_button').invoke('show');
+  $$('.new_button').invoke('hide');
+  if(!$('popup_bucket').visible()) {
+    try {
+      new Effect.SlideDown("popup_bucket",{duration:0.33});
+    } catch (e) {
+      alert('RJS error:\n\n' + e.toString()); alert('new Effect.SlideDown(\"popup_bucket\",{duration:0.33});');
+      throw e;
+    }
+  }
+}
+
+function hideBucketForm() {
+  $$('.cancel_button').invoke('hide');
+  $$('.new_button').invoke('show');
+  if($('popup_bucket').visible()) {
+    try {
+      new Effect.SlideUp("popup_bucket",{duration:0.33});
+    } catch (e) {
+      alert('RJS error:\n\n' + e.toString()); alert('new Effect.SlideDown(\"popup_bucket\",{duration:0.33});');
+      throw e;
+    }
+  }
+}
+
+function editBucketForm(id, tag, desc, milestone_id, state, boilerplate) {
   var form = $('new_bucket');
+  form.reset();
   form['bucket_tag'].value = tag;
   form['bucket_description'].value = desc;
   form['bucket_state'].value = state;
   form['bucket_milestone_id'].value = milestone_id;
   form['bucket_boilerplate'].value = boilerplate;
+  form['bucket_id'].value = id;
+  form['bucket_submit'].value = 'Save';
+
+  showBucketForm();
+}
+
+function newBucketForm() {
+  var form = $('new_bucket');
+  form.reset();
+  form['bucket_id'].value = '';
+  form['bucket_submit'].value = 'Create';
 
   showBucketForm();
 }
