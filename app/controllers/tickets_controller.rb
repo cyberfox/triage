f18d82d832993df1dc16bb6678188b7262116ce4
Morgan Schweers <cyberfox@gmail.com>
2009-04-17 03:24:41 -0700

    Whoa...make it walk through ticket search results.  Niiiice.  VERY niiice.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,12 +10,15 @@ class TicketsController < ApplicationController
     @bin = @lh_project.bins.find {|bin| bin.id.to_s == params[:bin_id].to_s}
     @lh_tickets = Ticket.search(db_project, @bin.query)
     prep_bucket_form
+    session[:tickets] = @lh_tickets.collect(&:number)
+    session[:ticket_index] = 0
   end
 
   def show
     @lh_project = Project.find_by_lighthouse_project(current_user, params[:project_id])
     db_project = current_user.projects.find_by_lighthouse_id(@lh_project.id)
     @lh_ticket = db_project.tickets.find_by_number(params[:id]).lighthouse
+    session[:ticket_index] = session[:tickets].index(@lh_ticket.number) if session[:tickets]
     session[:ticket_number] = @lh_ticket.number
 
     users = (@lh_ticket.versions.collect(&:user_id) +
@@ -33,8 +36,17 @@ class TicketsController < ApplicationController
     # Pull the current search from the session, and get the next entry, and show it.
   end
 
+
+  # Pull the current search from the session, get the next entry, and show it.
   def next
-    # Pull the current search from the session, get the next entry, and show it.
+    if session[:ticket_index]
+      next_ticket_idx = session[:ticket_index].to_i + 1
+      if next_ticket_idx > session[:tickets].length
+        # TODO -- Retrieve next 30 tickets?
+      end
+      next_ticket_number = session[:tickets][next_ticket_idx]
+      redirect_to :action => 'show', :project_id => params[:project_id], :id => next_ticket_number
+    end
   end
 
   def search
