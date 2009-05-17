022bc4d8053c6c8a92dd2c593a46a6635258aff1
Morgan Schweers <cyberfox@gmail.com>
2009-05-17 02:03:21 -0700

    Use a helper for the grossly chained method being used in the search results to get the body; nerf said helper so it works in tests.

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,10 @@
 module TicketsHelper
+  def get_first_body(project_id, ticket_number)
+    versions = current_user.projects.find_by_lighthouse_id(project_id).tickets.find_by_number(ticket_number).lighthouse.versions
+    if versions
+      versions.first.body
+    else
+      ""
+    end
+  end
 end
