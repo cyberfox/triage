ab6bc9905429eff3b0c7b32d905466f7d6154841
Morgan Schweers <cyberfox@gmail.com>
2010-09-11 17:28:43 -0700

    Force the bucket apply code to retrieve the ticket 'fresh' from Lighthouse before applying changes, and then save the updated ticket to the database.

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -5,11 +5,12 @@ class Bucket < ActiveRecord::Base
   validates_presence_of :user_id, :tag
 
   def apply_one(ticket)
-    lh_ticket = ticket.lighthouse
+    lh_ticket = Ticket.retrieve(ticket.project, ticket.number)
     lh_ticket.milestone_id = milestone.lighthouse_id if milestone
     lh_ticket.state = state if state
     lh_ticket.body = boilerplate if boilerplate
     lh_ticket.tags << tag
     lh_ticket.save
+    ticket.update_attribute :data, lh_ticket.to_xml
   end
 end
